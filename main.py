4da6a4913203fd4c349618bacdf3810071d3d90f
Dilan Sarith Hewawitharana <42795493+dilanHewawitharana@users.noreply.github.com>
2020-05-27 13:07:07 +0900

    Update main.py for new model

--- main.py
+++ main.py
@@ -33,7 +33,6 @@ import paho.mqtt.client as mqtt
 
 from argparse import ArgumentParser
 from inference import Network
-import numpy as np
 
 # MQTT server environment variables
 HOSTNAME = socket.gethostname()
@@ -42,9 +41,17 @@ MQTT_HOST = IPADDRESS
 MQTT_PORT = 3001
 MQTT_KEEPALIVE_INTERVAL = 60
 
-#count history
+check_in_cnt_check = 0;
+check_out_cnt_check = 0;
 
-count_history = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+current_count = 0
+total_count = 0
+
+timer_on = False
+timer_start = None
+timer_end = None
+duration = 0
+
 
 def build_argparser():
     """
@@ -72,40 +79,62 @@ def build_argparser():
                         "(0.5 by default)")
     return parser
 
-
 def connect_mqtt():
     ### TODO: Connect to the MQTT client ###
     client = mqtt.Client()
     client.connect(MQTT_HOST, MQTT_PORT, MQTT_KEEPALIVE_INTERVAL)
+
     return client
 
 def draw_boxes(frame, result, prob_threshold, width, height):
     '''
     Draw bounding boxes onto the frame.
     '''  
-    count = 0
     probs = result[0, 0, :, 2]
     for i, p in enumerate(probs):
         if p > prob_threshold:
-            count += 1
             box = result[0, 0, i, 3:]
-            p1 = (int(box[0] * width), int(box[1] * height))
-            p2 = (int(box[2] * width), int(box[3] * height))
-            frame = cv2.rectangle(frame, p1, p2, (0, 0, 255), 2)
-    return count
+            p1_x = int(box[0] * width)
+            p1_y = int(box[1] * height)
+            p2_x = int(box[2] * width)
+            p2_y = int(box[3] * height)
+            centre_x = (p1_x + p2_x)/2
+            centre_Y = (p1_y + p2_y)/2
+            check_people_in(frame, centre_Y)
+            check_people_out(frame, centre_x)
+            frame = cv2.rectangle(frame, (p1_x, p1_y), (p2_x, p2_y), (0, 0, 255), 2)
 
-def calculate_current_count(count): 
-    # calculate the mean of latest 10 values and rounded into nearest integer
-    index = 9
-    while index > 0:
-      count_history[index] = count_history[index-1]
-      index -= 1
+def check_people_in(frame, centre_Y):
+    height = frame.shape[0]
+    width = frame.shape[1]
+    bottom_margin = int(height*0.7)
+    #cv2.line(frame, (0, bottom_margin), (width, bottom_margin), (0, 255, 0), thickness=2)
         
-    count_history[0] = count
-    mean = statistics.mean(count_history) 
-    
-    return int(round(mean))
+    global check_in_cnt_check
+    global current_count
+    if(check_in_cnt_check > 0):
+        check_in_cnt_check -= 1
+    elif(centre_Y > bottom_margin):
+        current_count += 1
+        check_in_cnt_check = 50
 
+        
+def check_people_out(frame, centre_x):
+    height = frame.shape[0]
+    width = frame.shape[1]
+    right_margin = int(width*0.85)
+    #cv2.line(frame, (right_margin, 0), (right_margin, height), (0, 255, 0), thickness=2)
+    
+    global check_out_cnt_check
+    global current_count
+    global total_count
+    
+    if(check_out_cnt_check > 0):
+        check_out_cnt_check -= 1
+    elif(centre_x > right_margin):
+        current_count -= 1
+        total_count += 1
+        check_out_cnt_check = 50
 
 def infer_on_stream(args, client):
     """
@@ -120,16 +149,16 @@ def infer_on_stream(args, client):
     infer_network = Network()
     # Set Probability threshold for detections
     prob_threshold = args.prob_threshold
-    
+
     model = args.model
     
     DEVICE = args.device
     CPU_EXTENSION = args.cpu_extension
-
+    
     ### TODO: Load the model through `infer_network` ###
     infer_network.load_model(model, CPU_EXTENSION, DEVICE)
-    network_shape = infer_network.get_input_shape()
-
+    net_input_shape = infer_network.get_input_shape()
+    
     ### TODO: Handle the input stream ###
     # Camera input stream
     if args.input == 'CAM':
@@ -147,64 +176,38 @@ def infer_on_stream(args, client):
         
     cap = cv2.VideoCapture(input_validated)
     cap.open(input_validated)
-
+    
+    # Grab the shape of the input 
     width = int(cap.get(3))
     height = int(cap.get(4))
 
-    in_shape = network_shape['image_tensor']
-
-    #iniatilize variables
-    request_id = 0
-    
-    current_count = 0
-    pre_current_count = 0
-    
-    total_count = 0
-    
-    timer_on = False
-    timer_start = None
-    timer_end = None
-    duration = 0
-
     ### TODO: Loop until stream is over ###
     while cap.isOpened():
-
         ### TODO: Read from the video capture ###
         flag, frame = cap.read()
         if not flag:
             break
 
         ### TODO: Pre-process the image as needed ###
-        image = cv2.resize(frame, (in_shape[3], in_shape[2]))
-        image_p = image.transpose((2, 0, 1))
-        image_p = image_p.reshape(1, *image_p.shape)
-
+        p_frame = cv2.resize(frame, (net_input_shape[3], net_input_shape[2]))
+        p_frame = p_frame.transpose((2,0,1))
+        p_frame = p_frame.reshape(1, *p_frame.shape)
+        
         ### TODO: Start asynchronous inference for specified request ###
-        net_input = {'image_tensor': image_p,'image_info': image_p.shape[1:]}
+        infer_network.exec_net(p_frame)
         average_duration = None
-        infer_network.exec_net(net_input, request_id)
 
         ### TODO: Wait for the result ###
         if infer_network.wait() == 0:
-
             ### TODO: Get the results of the inference request ###
-            net_output = infer_network.get_output()
-
-            ### TODO: Extract any desired stats from the results ###
-            pre_current_count = current_count  
-            obj_count = draw_boxes(frame, net_output, prob_threshold, width, height)
-            current_count = calculate_current_count(obj_count)
-    
-            ### TODO: Calculate and send relevant information on ###
-            ### current_count, total_count and duration to the MQTT server ###
-            ### Topic "person": keys of "count" and "total" ###
-            ### Topic "person/duration": key of "duration" ###
-            
-            #Total count update
-            if pre_current_count > current_count:
-                total_count += (pre_current_count - current_count)
-            
+            result = infer_network.get_output()
+            # Draw the output mask onto the input 
+            draw_boxes(frame, result, prob_threshold, width, height)
+        
             #Time duration calculate
+            global timer_on
+            global current_count
+            global duration
             if (current_count > 0) and (timer_on == False): #Timer on if there are people detedted
                 timer_on = True
                 timer_start = time.time()
@@ -214,6 +217,11 @@ def infer_on_stream(args, client):
                 duration += timer_end - timer_start
                 average_duration = duration/total_count
 
+            ### TODO: Calculate and send relevant information on ###
+            ### current_count, total_count and duration to the MQTT server ###
+            ### Topic "person": keys of "count" and "total" ###
+            ### Topic "person/duration": key of "duration" ###
+            
             client.publish('person',
                            payload=json.dumps({
                                'count': current_count, 'total': total_count}),
@@ -223,16 +231,19 @@ def infer_on_stream(args, client):
                                payload=json.dumps({'duration': average_duration}),
                                qos=0, retain=False)
                 
-        ### TODO: Send the frame to the FFMPEG server ###
+            cv2.putText(frame, "Current Count : %d" %current_count, (15, 15), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 255), 1)
+            cv2.putText(frame, "Total Count : %d" %total_count, (15, 35), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 255), 1)
 
+        ### TODO: Send the frame to the FFMPEG server ###
         ### TODO: Write an output image if `single_image_mode` ###
-        frame = cv2.resize(frame, (768, 432))
         sys.stdout.buffer.write(frame)
         sys.stdout.flush()
-        
+            
+    # Release the capture and destroy any OpenCV windows
     cap.release()
     cv2.destroyAllWindows()
-
+    ### TODO: Disconnect from MQTT
+    client.disconnect()
 
 def main():
     """
